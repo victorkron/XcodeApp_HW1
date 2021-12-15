3dc98ba41ced27c97d5268ba0ac0753b94ff99f7
Karim <karimruabhi@MacBook-Pro-Karim.local>
2021-12-15 21:33:59 +0300

    weatherApp v.1.0

---
+++ WeatherApp/ViewControllers/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Карим Руабхи on 15.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBAction func logInButtonPressed(_ sender: Any) {
+        print(userNameTextField.text)
+        print(userPasswordTextField.text)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView
+            .addGestureRecognizer(
+                UITapGestureRecognizer(
+                    target: self,
+                    action: #selector(hideKeyboard)))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#function)
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.keyboardWasShown),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.keyboardWillBeHidden(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+    
+    
+    @objc func keyboardWasShown(notification: Notification) {
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue)
+            .cgRectValue
+            .size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        UIView.animate(withDuration: 1) {
+            self.scrollView.constraints
+                .first(where: { $0.identifier == "keyboardShown" })?
+                .priority = .required
+            self.scrollView.constraints
+                .first(where: { $0.identifier == "keyboardHide" })?
+                .priority = .defaultHigh
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+        UIView.animate(withDuration: 1) {
+            self.scrollView.constraints
+                .first(where: { $0.identifier == "keyboardShown" })?
+                .priority = .defaultHigh
+            self.scrollView.constraints
+                .first(where: { $0.identifier == "keyboardHide" })?
+                .priority = .required
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func hideKeyboard() {
+        self.scrollView?.endEditing(true)
+    }
+
+}
+
